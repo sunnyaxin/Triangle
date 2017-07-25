17700246a4fd7bcb85c1a882d5f22724cc5b3a61
Yaxin Wang <yxWang@CNyxWang.local>
2017-07-25 15:39:46 +0800

    Add draw line, vertical and horizontal.

---
+++ src/main/java/Triangle.java
@@ -0,0 +1,23 @@
+class Triangle {
+    Triangle() {
+    }
+
+    String drawEasiest() {
+        return "*";
+    }
+
+    String drawLine(int n, String type) {
+        String content = getContent(type);
+        StringBuilder stringBuilder = new StringBuilder(n);
+        for (int i = 0; i < n; i++) {
+            stringBuilder.append(content);
+        }
+        return stringBuilder.toString();
+    }
+
+    private String getContent(String type) {
+        if (type.equals("H"))
+            return "*";
+        return "*/n";
+    }
+}
---
+++ src/test/java/TriangleTest.java
@@ -0,0 +1,33 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TriangleTest {
+
+    private final Triangle triangle = new Triangle();
+
+    @Test
+    public void test_one_asterisk() throws Exception {
+        assertEquals("*", triangle.drawEasiest());
+    }
+
+    @Test
+    public void test_draw_horizontal_line_when_n_is_3() throws Exception {
+        assertEquals("***", triangle.drawLine(3, "H"));
+    }
+
+    @Test
+    public void test_draw_horizontal_line_when_n_is_8() throws Exception {
+        assertEquals("********", triangle.drawLine(8, "H"));
+    }
+
+    @Test
+    public void test_draw_vertical_line_when_n_is_2() throws Exception {
+        assertEquals("*/n*/n", triangle.drawLine(2, "V"));
+    }
+
+    @Test
+    public void test_draw_vertical_line_when_n_is_4() throws Exception {
+        assertEquals("*/n*/n*/n*/n", triangle.drawLine(4, "V"));
+    }
+}
